fefb65ca641d0ab3e8c9734f2c02f70dd4e3cc0e
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2023-06-01 19:37:52 -0300

    Preparando solução para o desenvolvimento futuro da aplicação.

---
+++ App_Disk_Pizza_Nostra_Casa/Service/Data_Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Disk_Pizza_Nostra_Casa.Service
+{
+
+    public class Data_Service
+    {
+
+        // Classe destinada para uma futura integração a uma API.
+
+    }
+
+}
