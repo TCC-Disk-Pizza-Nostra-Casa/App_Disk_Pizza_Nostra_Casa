17b9ab3452cd3cd302ac943039000f0df5d7b7bd
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-09-18 14:51:35 -0300

    Adicionando Model e DataService de Fornecedor.

---
+++ App_Disk_Pizza_Nostra_Casa/Model/Fornecedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Disk_Pizza_Nostra_Casa.Model
+{
+    public class Fornecedor
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string cnpj {  get; set; }
+        public string telefone { get; set; }
+        public DateTime data_cadastro { get; set; }
+        public DateTime data_modificacao { get; set; }
+        public bool ativo{ get;  set; }
+    }
+}
---
+++ App_Disk_Pizza_Nostra_Casa/Service/Data_Service_Fornecedor.cs
@@ -0,0 +1,65 @@
+﻿using App_Disk_Pizza_Nostra_Casa.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Disk_Pizza_Nostra_Casa.Service
+{
+    public class Data_Service_Fornecedor : Data_Service
+    {
+        public static async Task<Fornecedor>? SaveAsyncFornecedor(Fornecedor model)
+        {
+
+            var post_json = JsonConvert.SerializeObject(model);
+
+            string json = await Data_Service.SendDataApi(post_json, "/fornecedor/save");
+
+            Fornecedor model_retornada = JsonConvert.DeserializeObject<Fornecedor>(json);
+
+            return model_retornada;
+
+        }
+
+        public static async Task<bool>? DeleteAsyncFornecedor(int id)
+        {
+
+            var post_json = JsonConvert.SerializeObject(id);
+
+            string json = await Data_Service.SendDataApi(post_json, "/fornecedor/delete");
+
+            bool resultado = JsonConvert.DeserializeObject<bool>(json);
+
+            return resultado;
+
+        }
+
+        public static async Task<List<Fornecedor>>? GetListAsyncFornecedor()
+        {
+
+            string json = await Data_Service.GetDataApi("/fornecedor/list");
+
+            List<Fornecedor> model_retornada = JsonConvert.DeserializeObject<List<Fornecedor>>(json);
+
+            return model_retornada;
+
+        }
+
+        public static async Task<List<Fornecedor>>? SearchAsyncFornecedor(string parametro)
+        {
+
+            var post_json = JsonConvert.SerializeObject(parametro);
+
+            string json = await Data_Service.SendDataApi(post_json, "/fornecedor/search");
+
+            List<Fornecedor> model_retornada = JsonConvert.DeserializeObject<List<Fornecedor>>(json);
+
+            return model_retornada;
+
+        }
+
+
+    }
+}
